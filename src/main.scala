9d526888b9c02c0c17ee1e882064aaa7dba577a7
Albin Arvidsson <albin.arvidsson@visma.com>
2022-02-10 13:53:06 +0100

    Can read sudoku from file

--- src/main.scala
+++ src/main.scala
@@ -1,4 +1,6 @@
-case class Cell(number: Option[Int])
+import scala.io.Source
+
+case class Cell(value: Option[Int])
 case class Row(cells: List[Cell])
 case class Sudoku(rows: List[Row])
 
@@ -6,4 +8,24 @@ def allBlankSudoku() : Sudoku =
   Sudoku(List.fill(9)(Row(List.fill(9)(Cell(None)))))
 
 def isSudoku(candidate: Sudoku) : Boolean =
-  false
+  candidate.rows.forall(row => row.cells.forall(cell => cell.value.isEmpty || (1 <= cell.value.get && cell.value.get <= 9)))
+
+def isFilled(sudoku: Sudoku) : Boolean =
+  sudoku.rows.forall(row => row.cells.forall(cell => cell.value.nonEmpty))
+
+def printSudoku(sudoku: Sudoku) : Unit =
+  sudoku.rows.foreach( row => println(row.cells.map(_.value.getOrElse(".")).mkString))
+
+def readSudokuFromFile(path: String) : Sudoku =
+  val lines = Source.fromFile(path).getLines.toList
+  Sudoku(lines.map(line => Row(line.map(char => if (char == '.') Cell(None) else Cell(Option(char.asDigit))).toList)))
+
+
+@main def main() =
+//  val sudoku =
+  val path = "src/resources/exampleSudoku"
+  printSudoku(readSudokuFromFile(path))
+//  println(isSudoku(blankSudoku))
+//  println(isFilled(blankSudoku))
+
+
