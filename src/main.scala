6762191445de270b4ab758673f64ed41c7d7ea70
Ivar Josefsson <ivar.josefsson@gmail.com>
2022-02-10 12:47:17 +0100

    Remove braces

--- src/main.scala
+++ src/main.scala
@@ -2,10 +2,8 @@ case class Cell(number: Option[Int])
 case class Row(cells: List[Cell])
 case class Sudoku(rows: List[Row])
 
-def allBlankSudoku() : Sudoku = {
+def allBlankSudoku() : Sudoku =
   Sudoku(List.fill(9)(Row(List.fill(9)(Cell(None)))))
-}
 
-def isSudoku(candidate: Sudoku) : Boolean = {
+def isSudoku(candidate: Sudoku) : Boolean =
   false
-}
