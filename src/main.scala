bd609da00d1c7a5efedc821bc0d8d9e55da53b37
Ivar Josefsson <ivar.josefsson@gmail.com>
2022-02-10 12:46:18 +0100

    allBlank

--- src/main.scala
+++ src/main.scala
@@ -1,4 +1,11 @@
 case class Cell(number: Option[Int])
-case class Row(cell: Cell)
-case class Sudoku(row: Row)
+case class Row(cells: List[Cell])
+case class Sudoku(rows: List[Row])
 
+def allBlankSudoku() : Sudoku = {
+  Sudoku(List.fill(9)(Row(List.fill(9)(Cell(None)))))
+}
+
+def isSudoku(candidate: Sudoku) : Boolean = {
+  false
+}
